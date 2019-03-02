d9bbb34a49774ce2cff507222ebf2ffdce95a12c
Petros Grammatikopoulos <pgrammatikopoulos@citycollege.sheffield.eu>
2019-03-02 02:44:32 +0200

    Add files via upload

--- lcm.c
+++ lcm.c
@@ -1,54 +1,54 @@
 ﻿#include <stdio.h>
 
-long populationPrompt();
-long scanNumber(long i);
-long leastCommonMultiple(long *arr, long population);
-long pairMultiple(long x, long y);
+long long populationPrompt();
+long long scanNumber(long long i);
+long long leastCommonMultiple(long long *arr, long long population);
+long long pairMultiple(long long x, long long y);
 
 int main(void){
-printf("This utility calculates the least common multiple of an infinite (theoretically)\namount of positive numbers (values up to 2^31-1)\n");
+printf("This utility calculates the least common multiple of an infinite (theoretically)\namount of positive numbers (values up to 2^63-1)\n");
 
-long population = populationPrompt();
-long numbers[population];
+long long population = populationPrompt();
+long long numbers[population];
 
 for (int i = 0; i < population; i++) {
 	numbers[i] = scanNumber(i);
 }
 
-long lcm = leastCommonMultiple(numbers, population);
-printf("------------------------------------\nThe least common multiple is: %li\n------------------------------------\n", lcm);
+long long lcm = leastCommonMultiple(numbers, population);
+printf("------------------------------------\nThe least common multiple is: %lli\n------------------------------------\n", lcm);
 getchar();
 fputs("Press ENTER to terminate the utility",stdout);
 getchar();
 }
 
-long populationPrompt(){
-		long population = 0;
+long long populationPrompt(){
+		long long population = 0;
 		while (population <= 0) {
 			printf("How many numbers are you\ngoing to find the LCM for? : ");
-			scanf( "%li", &population);
+			scanf( "%lli", &population);
 		}
 		return population;
 }
 
-long scanNumber(long i) {
-		long x;
+long long scanNumber(long long i) {
+		long long x;
 		do {
-			printf("Give me number %li: ",i+1);
-			scanf( "%li", &x);
+			printf("Give me number %lli: ",i+1);
+			scanf( "%lli", &x);
 		} while (x <= 0);
 		return x;
 }
 
-long leastCommonMultiple(long *arr, long population) {
-		for (long i = 1; i < population; i++)
+long long leastCommonMultiple(long long *arr, long long population) {
+		for (long long i = 1; i < population; i++)
 			arr[i] = pairMultiple(arr[i - 1], arr[i]);
 		return arr[population - 1];
 }
 
-long pairMultiple(long x, long y) {
-		long count = 1;
-		while (count <= 2147483647) {
+long long pairMultiple(long long x, long long y) {
+		long long count = 1;
+		while (count <= 9223372036854775807) {
 			if (count % x == 0 && count % y == 0)
 				break;
 			count++;
